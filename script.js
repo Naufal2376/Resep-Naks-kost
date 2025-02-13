b197809a660ffecfb8576fc2d908ef3595e1e24a
Naufal <naufalbhurr@gmail.com>
2025-02-13 11:34:10 +0700

    update web

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.querySelector(".navbar")
+  const searchBox = document.querySelector(".search-box")
   window.addEventListener("scroll", function () {
     if (window.scrollY > 50) {
       navbar.style.background = "rgba(255, 255, 255, 0.8)"
+      navbar.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.1)"
+      searchBox.style.background = "rgba(255, 255, 255, 0.8)"
     } else {
       navbar.style.background = "white"
+      navbar.style.boxShadow = "none"
+      searchBox.style.background = "#ebe8e8"
     }
   })
 
